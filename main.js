4dcbd53fd1ff8b0a9d2bfd0ca22de5d1727ad722
AndreaDuh <xAsianRejectx@gmail.com>
2015-11-24 17:14:04 -0800

    Innitial commit

---
+++ main.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+	$('#search-term').submit(function(event){
+    event.preventDefault();
+    var searchTerm = $('#query').val();
+    getRequest(searchTerm);
+    })
+});
+
+   	function getRequest(searchTerm) {
+		var params = {
+			part: 'snippet',
+      key: "AIzaSyAV6MvqqEA1FvWU9OUxHjGIyEgo3f5Yz8I",
+      q: encodeURIComponent($('#search-term').val()).replace(/%20/g, '+'),
+      s: searchTerm
+		};
+		url = 'https://www.googleapis.com/youtube/v3/search';
+		$.getJSON(url, params, function(data){
+	    showResults(data.Search);
+	    });
+	}
+
+  function showResults(results){
+  var html = "";
+  $.each(results, function(index,value){
+    html += '<p>' + value.Title + '</p>';
+    console.log(value.Title);
+  });
+  $('#search-results').html(html);
+}
