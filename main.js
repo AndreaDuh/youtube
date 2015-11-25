4908f25b899ba4e48dd674e43f982a6672349b30
AndreaDuh <xAsianRejectx@gmail.com>
2015-11-24 17:53:59 -0800

    Update main.js
    Fixed spacing.

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
 $(document).ready(function(){
-	$('#search-term').submit(function(event){
+    $('#search-term').submit(function(event){
     event.preventDefault();
     var searchTerm = $('#query').val();
-    getRequest(searchTerm);
+      getRequest(searchTerm);
     })
 });
 
    	function getRequest(searchTerm) {
-		var params = {
-			part: 'snippet',
-      key: "AIzaSyAV6MvqqEA1FvWU9OUxHjGIyEgo3f5Yz8I",
-      q: encodeURIComponent($('#search-term').val()).replace(/%20/g, '+'),
-      s: searchTerm
-		};
-		url = 'https://www.googleapis.com/youtube/v3/search';
-		$.getJSON(url, params, function(data){
+	  var params = {
+	    part: 'snippet',
+            key: "AIzaSyAV6MvqqEA1FvWU9OUxHjGIyEgo3f5Yz8I",
+            q: encodeURIComponent($('#search-term').val()).replace(/%20/g, '+'),
+            s: searchTerm
+	};
+	  url = 'https://www.googleapis.com/youtube/v3/search';
+	  $.getJSON(url, params, function(data){
 	    showResults(data.Search);
-	    });
+	  });
 	}
 
   function showResults(results){
